7b17dc8b91ee813917533e033034ca3c42457d26
catplane <nihaoyawoshixixida@gmail.com>
2019-01-09 20:33:07 +0800

    关于namespace和reverse的用法

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^index/$', views.index)
+    url(r'^users/index/$', views.index, name='index'),
+    url(r'^say/$', views.say, name='say'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def index(request):
     return HttpResponse("hello world")
+
+
+def say(request):
+    # 对于未指明namespace的，reverse(路由name)
+    # 对于指明namespace的，reverse(命名空间namespace: 路由name)
+    url = reverse('index')
+    print(url)
+    return HttpResponse('say')
