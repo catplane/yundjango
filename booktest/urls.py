fe5bfdc0358fdf061b7d2f490993a6836b974aa6
catplane <nihaoyawoshixixida@gmail.com>
2019-01-17 09:52:22 +0800

    Mixin的部份应用

--- booktest/urls.py
+++ booktest/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # url(r'^books/$', views.BooksAPIVIew.as_view()),
     # url(r'^books/(?P<pk>\d+)/$', views.BookAPIView.as_view())
     url(r'^books/$', views.BookListView.as_view()),
+    # url(r'^books/(?P<pk>\d+)/$', views.BookDetailView.as_view()),
 ]
 
 # router = DefaultRouter()
--- booktest/views.py
+++ booktest/views.py
@@ -1,6 +1,8 @@
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import BookInfoSerializer
 from .models import BookInfo
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 
 # class BookInfoViewSet(ModelViewSet):
 #     queryset = BookInfo.objects.all()
@@ -11,15 +13,50 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class BookListView(APIView):
-    def get(self, request):
-        books = BookInfo.objects.all()
-        serializer = BookInfoSerializer(books, many=True)
-        return Response(serializer.data)
+# class BookListView(APIView):
+#     def get(self, request):
+#         books = BookInfo.objects.all()
+#         serializer = BookInfoSerializer(books, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         json_str = request.data
+#         book = BookInfoSerializer(data=json_str)
+#         # book.is_valid(raise_exception=True)
+#         book.save()
+#         return Response('xixi')
 
-    def post(self, request):
-        json_str = request.data
-        book = BookInfoSerializer(data=json_str)
-        # book.is_valid(raise_exception=True)
-        book.save()
-        return Response('xixi')
+# class BookListView(ListModelMixin, GenericAPIView):
+#     queryset = BookInfo.objects.all()
+#     serializer_class = BookInfoSerializer
+#
+#     def get(self, request):
+#         return self.list(request)
+
+
+# class BookDetailView(GenericAPIView):
+#     queryset = BookInfo.objects.all()
+#     serializer_class = BookInfoSerializer
+#
+#     def get(self, request, pk):
+#         book = self.get_object() # get_object()方法根据pk参数查找queryset中的数据对象
+#         serializer = self.get_serializer(book)
+#         # print(serializer)
+#         return Response(serializer.data)
+
+
+# class BookDetailView(RetrieveModelMixin, GenericAPIView):
+#     queryset = BookInfo.objects.all()
+#     serializer_class = BookInfoSerializer
+#
+#     def get(self, request, pk):
+#         return self.retrieve(request)
+
+class BookListView(ListAPIView):
+    queryset = BookInfo.objects.all()
+    serializer_class = BookInfoSerializer
+
+
+
+
+
